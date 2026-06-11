4390d886fa39b8fc7bcddf061b2cb74f151cdb68
Adithya Sudheer Karat <31129473+adithyasudheer@users.noreply.github.com>
2026-06-11 20:23:13 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,48 @@
+class DSU
+{
+    int []parent;
+    DSU(int n)
+    {
+        parent=new int[n];
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+    }
+    int findParent(int x)
+    {
+        if(parent[x]==x)
+            return x;
+        return parent[x]=findParent(parent[x]);
+    }
+    boolean union(int x, int y)
+    {
+        int parx=findParent(x);
+        int pary=findParent(y);
+        if(parx!=pary)
+        {
+            parent[Math.min(parx,pary)]=Math.max(parx,pary);
+            return true;
+        }
+        return false;
+    }
+}
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) 
+    {
+        int n=0;
+        for(int i=0;i<edges.length;i++)
+        {
+            n=Math.max(Math.max(edges[i][0],edges[i][1]),n);
+        }
+        DSU dsuobj=new DSU(n+1);
+        int []res=new int[2];
+        for(int i=0;i<edges.length;i++)
+        {
+            if(!dsuobj.union(edges[i][0],edges[i][1]))
+            {
+                res[0]=edges[i][0];
+                res[1]=edges[i][1];
+            }
+        }
+        return res;
+    }
+}
