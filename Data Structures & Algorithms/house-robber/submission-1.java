900a133e4d22bfa18cdd14a6c1d3f12591ac98c1
Adithya Sudheer Karat <31129473+adithyasudheer@users.noreply.github.com>
2026-06-30 03:10:24 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    int []dp;
+    int helper(int []nums, int idx)
+    {
+        if(idx<0)
+            return 0;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        int take=Integer.MIN_VALUE;
+        int notake=Integer.MIN_VALUE;
+        
+        take=nums[idx];
+        if(idx-2>=0)
+            take+=helper(nums,idx-2);
+        if(idx-1>=0)
+            notake=helper(nums,idx-1);
+        return dp[idx]=Math.max(take,notake);
+    }
+    public int rob(int[] nums) 
+    {
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return helper(nums,nums.length-1);    
+    }
+}
